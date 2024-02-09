a62a05af5572eebcc0c9b9f05d9b7feb946b5706
Marno van der Maas <mvdmaas+git@lowrisc.org>
2024-02-09 10:42:54 +0000

    Make blinky actually blink

--- boot.cc
+++ boot.cc
@@ -7,6 +7,8 @@
 
 using namespace CHERI;
 
+#define GPIO_VALUE (0xF)
+
 /**
  * C++ entry point for the loader.  This is called from assembly, with the
  * read-write root in the first argument.
@@ -17,7 +19,12 @@ extern "C" uint32_t rom_loader_entry(void *rwRoot)
 	Capability<volatile uint32_t> gpio = root.cast<volatile uint32_t>();
 	gpio.address() = 0x80000000;
 	gpio.bounds() = sizeof(uint32_t);
+
+    uint32_t gpio_value = GPIO_VALUE;
 	while (true) {
-		*((volatile uint32_t *) gpio) = 0xF;
+        gpio_value ^= GPIO_VALUE;
+        for (int i = 0; i < 500; i++) {
+		    *((volatile uint32_t *) gpio) = gpio_value;
+        }
 	}
 }
